933343dd64b71e16911877ce9c8c254c92656af7
sebastien <sebastien.pingal@gmail.com>
2023-08-01 11:18:57 +0200

    feat(server.ts)

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app = express()
+
+const port = process.env.PORT ?? 3000
+
+app.use(express.json())
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
+
+// health check
+app.get('/', (req, res) => {
+  res.send('OK')
+})
