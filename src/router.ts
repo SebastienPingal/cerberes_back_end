305e301814cb9a7159c0094c18c1f30b4fba7155
sebastien <sebastien.pingal@gmail.com>
2023-08-15 15:31:25 +0200

    feat(router): syncDB

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,8 @@ import { Router } from 'express'
 import authRoutes from './entities/auth/auth.router'
 import usersRoutes from './entities/users/users.router'
 import passport from 'passport'
-import { testConnection, sequelize} from './utils/sequelize.client'
+import { testConnection } from './utils/sequelize.client'
+import { User, Contact, Conversation, UserConversation, Message } from '../sequelize/sequelize.models'
 
 const router = Router()
 router.use('/users', passport.authenticate('jwt', { session: false }), usersRoutes)
@@ -27,8 +28,14 @@ router.get('/testDB', async (req, res) => {
 
 // sync db
 router.get('/syncDB', async (req, res) => {
-    try {
-        await sequelize.sync({ force: true })
+  try {
+      console.log('syncing db')
+        await User.sync({force: true})
+        await Contact.sync({force: true})
+        await Conversation.sync({force: true})
+        await UserConversation.sync({force: true})
+        await Message.sync({ force: true })
+        console.log('synced db')
         res.send('OK')
     } catch (error) {
         const typedError = error as Error
