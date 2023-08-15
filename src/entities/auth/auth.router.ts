0fe7f1a706ecaf1ec2e127a565472c17654ebdc0
sebastien <sebastien.pingal@gmail.com>
2023-08-15 15:37:36 +0200

    feat(auth_router): login

--- src/entities/auth/auth.router.ts
+++ src/entities/auth/auth.router.ts
@@ -3,7 +3,7 @@ import auth_controller from './auth.controller'
 
 const router = express.Router()
 
-// router.post('/auth/login', auth_controller.login)
+router.post('/auth/login', auth_controller.login)
 router.post('/auth/register', auth_controller.register)
 
 export default router
